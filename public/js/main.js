76a080e62cf99b56374a23e60900cfdfa43cdd97
Steven Polido <steven.polido@vistage.com>
2019-12-04 11:05:44 -0800

    Moved script from the main view layout to the public directory.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,63 @@
-console.log('//DevBoard.io Node.js Applitcation ------------');
+console.log("//DevBoard.io Node.js Application ------------");
+
+const sendEmailAddress = document.querySelector(".send-email-address");
+sendEmailAddress.addEventListener("click", e => {
+  e.preventDefault();
+  const emailAddress = document.querySelector(".email").value;
+  const formData = new FormData(this);
+  fetch('send.js');
+});
+
+// Cookie Notification
+const cookiesAcceptBtn = document.querySelector(".cookies-accept-btn");
+const cookiesChangePreferencesBtn = document.querySelector(
+  ".cookies-change-preferences-btn"
+);
+const cookiesNotification = document.querySelector(".cookies-notification");
+cookiesAcceptBtn.addEventListener("click", () => {
+  console.log("cookies accept button clicked");
+  cookiesAcceptBtn.style.background = "#568c9b";
+  cookiesAcceptBtn.style.color = "#fff";
+
+  cookiesNotification.style.display = "none";
+});
+
+cookiesChangePreferencesBtn.addEventListener("click", () => {
+  console.log("cookies accept button clicked");
+  cookiesChangePreferencesBtn.style.background = "#568c9b";
+  cookiesChangePreferencesBtn.style.color = "#fff";
+});
+
+//if cookie hasn't been set...
+if (document.cookie.indexOf("CookieNotificationShown") < 0) {
+  // Display block / show modal
+  cookiesNotification.style.display = "block";
+
+  cookiesAcceptBtn.addEventListener("click", () => {
+    // Store cookie
+    document.cookie =
+      "CookieNotificationShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+  });
+} else {
+  // Display none on notification modal
+  cookiesNotification.style.display = "none";
+}
+
+// Scroll to top
+let basicScrollTop = function() {
+  // The button
+  let backToTopBtn = document.querySelector(".back-to-top-btn");
+
+  // Smooth scroll top
+  let topScrollTo = function() {
+    if (window.scrollY != 0) {
+      setTimeout(function() {
+        window.scrollTo(0, window.scrollY - 30);
+        topScrollTo();
+      }, 5);
+    }
+  };
+  // Listeners
+  backToTopBtn.addEventListener("click", topScrollTo);
+};
+basicScrollTop();

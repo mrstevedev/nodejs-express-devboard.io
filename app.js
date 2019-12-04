1d58996da1e2e6bad8ab0ec7b62fccaa4cb83f2b
Steven Polido <steven.polido@vistage.com>
2019-12-04 13:49:32 -0800

    Added helper function for time ago.

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
-const db = require('./config/database');
-const lessMiddleware = require('less-middleware');
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
+const db = require("./config/database");
+const lessMiddleware = require("less-middleware");
+const moment = require('moment');
 
 db.authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
   });
 
 const app = express();
 
 // Middleware for Handlebars
 // main is the template file to hold all the views eg. header, footer etc
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    helpers: {
+      fromNow: function(date) {
+        return moment(date).fromNow();
+      }
+    }
+  })
+);
+app.set("view engine", "handlebars");
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Static Folder 
-app.use(lessMiddleware(__dirname, '/public'));
-app.use(express.static(path.join(__dirname, '/public')));
+// Set Static Folder
+app.use(lessMiddleware(__dirname, "/public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Create Index Route
-app.get('/', (req, res, next) => {
-    res.redirect('/jobs');
+app.get("/", (req, res, next) => {
+  res.redirect("/jobs");
 });
 
 // About Route
-app.use('/about', require('./routes/about'));
+app.use("/about", require("./routes/about"));
 
 // Jobs Route
-app.use('/jobs', require('./routes/jobs'));
+app.use("/jobs", require("./routes/jobs"));
 
 // Register Route
-app.use('/register', require('./routes/register'));
+app.use("/register", require("./routes/register"));
 
-// Login Route 
-app.use('/login', require('./routes/login'));
+// Login Route
+app.use("/login", require("./routes/login"));
 
 const PORT = process.env.PORT || 5001;
 

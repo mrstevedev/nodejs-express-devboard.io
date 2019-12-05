cc0a2db1a6934bea39f21128c1016d85c1f39383
Steven Polido <steven.polido@vistage.com>
2019-12-05 00:17:57 -0800

    Updated model.

--- models/Job.js
+++ models/Job.js
@@ -1,25 +1,22 @@
-const Sequelize = require('sequelize').Sequelize;
-const db = require('../config/database');
+const Sequelize = require("sequelize").Sequelize;
+const db = require("../config/database");
 
-const Job = db.define('job', {
-    title: {
-        type: Sequelize.STRING
-    },
-    technologies: {
-        type: Sequelize.STRING
-    },
-    description: {
-        type: Sequelize.STRING
-    },
-    budget: {
-        type: Sequelize.STRING
-    },
-    company: {
-        type: Sequelize.STRING
-    },
-    contact_email: {
-        type: Sequelize.STRING
-    },
+const Job = db.define("job", {
+  title: {
+    type: Sequelize.STRING
+  },
+  tags: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  company: {
+    type: Sequelize.STRING
+  },
+  company_logo_ext: {
+    type: Sequelize.STRING
+  }
 });
 
 module.exports = Job;

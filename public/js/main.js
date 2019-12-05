af6434ebf26e6ead770b368ae08453867e13308e
Steven Polido <steven.polido@vistage.com>
2019-12-04 20:38:55 -0800

    Wrapped function call  with the debounce method.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,46 @@
 console.log("//DevBoard.io Node.js Application ------------");
 
+const searchJob = _.debounce(function (e, val) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `js/send.js?search=${e.target.value}`, true);
+
+  //   xhr.onload = function() {
+  //     if (this.status == 200) {
+  //       console.log(this.responseText);
+  //     }
+  //   }
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhr.send();
+}, 700);
+
+// readyState Values
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response is ready
+
+// HTTP Statuses
+// 200: "OK"
+// 403: "Forbidden"
+// 404: "Not Found"
+
+// Listener to search field
+const search = document.querySelector(".search-field");
+const searchForm = document.querySelector(".search-form");
+search.addEventListener("keyup", e => searchJob(e, search.value));
+searchForm.addEventListener("submit", e => e.preventDefault());
+
 const sendEmailAddress = document.querySelector(".send-email-address");
 sendEmailAddress.addEventListener("click", e => {
   e.preventDefault();
   const emailAddress = document.querySelector(".email").value;
   const formData = new FormData(this);
-  fetch('send.js');
+  fetch("send.js");
 });
 
 // Cookie Notification

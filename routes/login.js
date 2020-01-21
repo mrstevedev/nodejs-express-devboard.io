f7e9df5ba0dd5b28c4433e494553e7d698fc986c
Steven Polido <steven.polido@vistage.com>
2020-01-21 14:02:49 -0800

    Added /login/continue post route.

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+app = express();
+
+const mailer = require('express-mailer');
 
 router.get('/', (req, res) => {
     res.render('login', { path: 'login' });
+});
+
+router.post('/continue', (req, res) => {
+    res.render("continue", { path: 'continue' });
+    const email = req.body.email;
+    console.log(email);
 });
 
 module.exports = router;
